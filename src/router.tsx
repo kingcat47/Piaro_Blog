cf5cc01da96b4928cb08ce37f3a695952195bae7
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-09-13 04:52:18 +0900

    기본설정정

---
+++ src/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Home</div>,
+    },
+]);
+
+export default Router;
